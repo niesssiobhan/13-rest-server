c87eb8a8b8199a33163a67af0fed070715e8fcdb
niesssiobhan <niesssiobhan@live.com>
2019-01-22 18:04:39 -0800

    workijng on json and swagger

--- src/middleware/404.js
+++ src/middleware/404.js
@@ -1,5 +1,12 @@
 'use strict';
 
+/**
+ *
+ * error handling for the resource not found 
+ * @param {*} req request object
+ * @param {*} res response object
+ * @param {*} next run next function 
+ */
 module.exports = (req,res,next) => {
   let error = { error: 'Resource Not Found' };
   res.statusCode = 404;
